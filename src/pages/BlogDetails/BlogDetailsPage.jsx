ee7e9e42fd038229bac0f0031e11644ef9080a4d
Hasan Mt <hasan.2mahmud22@gmail.com>
2023-11-28 06:58:52 +0600

    feat: Limit (4)  related blogs show blog detail page sidebar and show tags of the single blog in the blog detail page

--- src/pages/BlogDetails/BlogDetailsPage.jsx
+++ src/pages/BlogDetails/BlogDetailsPage.jsx
@@ -8,7 +8,7 @@ import MainLayout from "../../components/MainLayout.jsx";
 
 import { images, stables } from "../../constants";
 import { useQuery } from "@tanstack/react-query";
-import {  getSinglePost } from "../../services/index/blogPosts.js";
+import {  getAllPosts, getSinglePost } from "../../services/index/blogPosts.js";
 import BlogsErrorMessage from "../../components/ErrorMessage/BlogsErrorMessage";
 
 import parseJsonToHtml from "../../utils/parsJsonToHtml.js";
@@ -22,6 +22,7 @@ const blogDetailsPage = () => {
   const [breadCrumbsData, setbreadCrumbsData] = useState([]);
   const [body, setBody] = useState(null);
   const userState = useSelector((state) => state.user);
+
   const { data, isLoading, isError } = useQuery({
     queryFn: () => getSinglePost({ slug }),
     queryKey: ["blog", slug],
@@ -35,7 +36,14 @@ const blogDetailsPage = () => {
     },
   });
 
+ const { data: BlogsData } = useQuery({
+    queryFn: () => getAllPosts(),
+    queryKey: ["posts"],
+  });
+  // Assuming postsData is an array of posts
+const limitedBlogs = BlogsData?.slice(0, 4);
 
+console.log("tags", data.tags)
   return (
     <MainLayout>
       {isLoading ? (
@@ -43,11 +51,14 @@ const blogDetailsPage = () => {
       ) : isError ? (
         <BlogsErrorMessage message="Couldn't fetch the post detail" />
       ) : (
-        <section className="container mx-auto max-w-5xl flex flex-col px-5 py-5 lg:flex-row lg:gap-x-5 lg:items-start">
+        <section className="container mx-auto max-w-6xl flex flex-col px-5 py-5 lg:flex-row lg:gap-x-5 lg:items-start">
+          
+            {/* left section in large device and top section in small device*/}
+    
           <article className="flex-1">
             <BreadCrumbs data={breadCrumbsData} />
             <img
-              className="rounded-xl w-full"
+              className="rounded-xl w-full object-cover object-center max-h-[460px] min-h-[300px]"
               src={
                 data?.photo
                   ? stables.UPLOAD_FOLDER_BASE_URL + data?.photo:
@@ -69,8 +80,13 @@ const blogDetailsPage = () => {
               postSlug={slug}
             />
           </article>
+          {/* right section in large device and bottom section in small device*/}
           <div>
-              <RelatedPosts></RelatedPosts>
+              <RelatedPosts
+                  blogs={limitedBlogs}
+                  tags={data?.tags}
+                  className="mt-8 lg:mt-0 lg:max-w-xs"
+              ></RelatedPosts>
             <div className="mt-7">
               <h2 className="font-roboto font-medium text-dark-hard mb-4 md:text-xl">
                 Share on:
--- src/pages/BlogDetails/Container/RelatedPosts.jsx
+++ src/pages/BlogDetails/Container/RelatedPosts.jsx
@@ -1,41 +1,68 @@
+/* eslint-disable react/prop-types */
 import { Link } from "react-router-dom";
-import { images } from "../../../constants";
+import { images, stables } from "../../../constants";
 
-const RelatedPosts = () => {
+const RelatedPosts = ({blogs = [], tags}) => {
+  console.log(blogs)
   return (
     <div
       className={`w-full shadow-[rgba(7,_65,_210,_0.1)_0px_9px_30px] rounded-lg p-4 mt-8 lg:mt-0 lg:max-w-xs`}
     >
       <h2 className="font-roboto font-medium text-dark-hard md:text-xl">
-       header
+       Latest blogs
       </h2>
       <div className="grid gap-y-5 mt-5 md:grid-cols-2 md:gap-x-5 lg:grid-cols-1">
        
+          {blogs.map((item) => (
           <div
+            key={item._id}
             className="flex space-x-3 flex-nowrap items-center"
           >
             <img
               className="aspect-square object-cover rounded-lg w-1/5"
-              src={images.HeroPhoto}
-              alt=""
+              src={
+                item?.photo
+                  ? stables.UPLOAD_FOLDER_BASE_URL + item?.photo
+                  : images.sampleBlogImage
+              }
+              alt={item.title}
             />
             <div className="text-sm font-roboto text-dark-hard font-medium">
               <h3 className="text-sm font-roboto text-dark-hard font-medium md:text-base lg:text-lg">
-                      realated post title
+                <Link to={`/blog/${item.slug}`}>{item.title}</Link>
               </h3>
               <span className="text-xs opacity-60">
-                 20 / 2
+                {new Date(item.createdAt).toLocaleDateString("en-US", {
+                  day: "numeric",
+                  month: "short",
+                  year: "numeric",
+                })}
               </span>
             </div>
           </div>
+        ))}
       </div>
       <h2 className="font-roboto font-medium text-dark-hard mt-8 md:text-xl">
         Tags
       </h2>
      
+       {tags.length === 0 ? (
         <p className="text-slate-500 text-xs mt-2">
           There is not tags for this post
         </p>
+      ) : (
+        <div className="flex flex-wrap gap-x-2 gap-y-2 mt-4">
+          {tags.map((item, index) => (
+            <Link
+              key={index}
+              to="/"
+              className="inline-block rounded-md px-3 py-1.5 bg-primary font-roboto text-xs text-white md:text-sm"
+            >
+              {item}
+            </Link>
+          ))}
+        </div>
+      )}
      
         <div className="flex flex-wrap gap-x-2 gap-y-2 mt-4">
         
